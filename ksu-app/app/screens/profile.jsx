8d5f3e3a0fd4c0a8eb5fe869310c9dc6496adcdf
dg205 <dante.galvan2004@gmail.com>
2025-04-06 18:30:57 -0400

    committing to work on different computer

--- ksu-app/app/screens/profile.jsx
+++ ksu-app/app/screens/profile.jsx
@@ -6,11 +6,21 @@ import {
     SafeAreaView,
     StyleSheet,
     Text,
+    Image,
+    View,
+    StyleSheet,
+    TouchableOpactity,
+    Switch, 
   } from "react-native";
   import React from "react";
+  const Profile = () => {
+    
+  }
   const Calendar = () => {
     return (
       <SafeAreaView>
+
+
 
       </SafeAreaView>
     );
